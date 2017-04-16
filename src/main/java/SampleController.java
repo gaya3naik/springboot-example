316b0036ab2932db595c85453a8f2e3f0abf4ae6
gnaik <gnaik@gainsight.com>
2017-04-16 16:48:52 +0530

    Spring boot example

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,23 @@
+/**
+ * Created by gnaik on 26/03/17.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello Gayathri!";
+    }
+
+    public static void main(String args[]) throws Exception{
+        SpringApplication.run(SampleController.class, args);
+    }
+}
